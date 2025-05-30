46a24a3fe3b0145105af49b6b4dffc3730ce8420
xtoshi999 <warm624.h5@gmail.com>
2025-05-30 10:31:18 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import React, { useState } from "react";
+import Layout from "@/layout/layout";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={`${styles.main} bg-[#0e0100]`}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+const GameBoard = () => {
 
-        <div className={styles.ctas}>
-        </div>
-      </main>
-    </div>
-  );
+    return (
+        <Layout>
+            <div className=" w-screen flex overflow-hidden">
+                
+                
+            </div>
+        </Layout>
+    );
 }
+
+export default GameBoard;
+
