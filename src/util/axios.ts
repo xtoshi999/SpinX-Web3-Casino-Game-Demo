9456d8b88c076bc9f34225854917ba7a76d6e9f3
xtoshi999 <warm624.h5@gmail.com>
2025-06-04 18:56:50 +0900

    Update axios.ts

--- src/util/axios.ts
+++ src/util/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const axiosServices = axios.create({
-  baseURL: `http://localhost:5000/api`,
+  baseURL: `https://casino.truebliss.dev/api`,
 });
 export default axiosServices;
